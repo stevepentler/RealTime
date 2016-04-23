c1f034f4ece60ac01f5025642d5e200d5da540ac
Steve Pentler <stevepentler@gmail.com>
2016-04-23 13:34:21 -0600

    Styles pages

--- test/server-test.js
+++ test/server-test.js
@@ -11,76 +11,82 @@ describe('Server', () => {
       options: [ 'black', 'brown' ] }
   }
 
-   // beforeEach(() => {
-   //    app.locals.surveys = {};
-   //  });
+    before((done) => {
+      this.port = 3000;
 
-  before((done) => {
-    this.port = 3000;
+      this.server = app.listen(this.port, (err, result) => {
+        if (err) { return done(err); }
+        done();
+      });
 
-    this.server = app.listen(this.port, (err, result) => {
-      if (err) { return done(err); }
-      done();
+      this.request = request.defaults({
+        baseUrl: 'http://localhost:3000/'
+      });
     });
 
-    this.request = request.defaults({
-      baseUrl: 'http://localhost:3000/'
+    after(() => {
+      this.server.close();
     });
-  });
 
   it('should exist', () => {
     assert(app);
   });
 
-  it('root should return a 200', (done) => {
-    this.request.get('/', (error, response) => {
-      if (error) { done(error); }
-      assert.equal(response.statusCode, 200);
-      done();
-    });
-  });
-
   describe('GET /', () => {
 
-    it('root should return a 200', (done) => {
+    it('should return a 200 for home page', (done) => {
       this.request.get('/', (error, response) => {
         if (error) { done(error); }
         assert.equal(response.statusCode, 200);
         done();
       });
     });
+
+    xit('should have a body with the name of the application', (done) => {
+      var title = app.locals.title;
+
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes(title),
+               `"${response.body}" does not include "${title}".`);
+        done();
+      });
+    });
   });
 
   describe('POST /admin', () => {
+    beforeEach(() => {
+      app.locals.surveys = {};
+    });
 
-    xit('should not return 404', (done) => {
-      console.log(surveyData)
-      this.request.post('/admin', {survey: surveyData}, (error, response) => {
+    it('should not return 404', (done) => {
+      this.request.post('/admin', { form: surveyData }, (error, response) => {
         if (error) { done(error); }
         assert.notEqual(response.statusCode, 404);
         done();
       });
     });
 
-    xit('should receive and store data', (done) => {
-      var validPizza = {
-        pizza: {
-          name: 'A vegan pizza',
-          toppings: [ 'mushrooms', 'onions', 'garlic', 'black olives' ]
-        }
-      };
-
-      this.request.post('/pizzas', { form: validPizza }, (error, response) => {
+    xit('should receive a survey and store it', (done) => {
+      app.locals.surveys = {};
+      this.request.post('/admin', { form: surveyData }, (error, response) => {
         if (error) { done(error); }
 
-        var pizzaCount = Object.keys(app.locals.pizzas).length;
-
-        assert.equal(pizzaCount, 1, `Expected 1 pizzas, found ${pizzaCount}`);
-
+        var surveyCount = Object.keys(surveys).length;
+        assert.equal(surveyCount, 1, `Expected 1 survey, found ${surveyCount}`);
         done();
       });
     });
 
-  });
+    it('should redirect the user to the admin results page', (done) => {
+      var payload = { survey: surveyData};
 
-});
+      this.request.post('/admin', { form: payload }, (error, response) => {
+        if (error) { done(error); }
+        var newSurveyId = Object.keys(app.locals.surveys)[0];
+        assert.equal(response.headers.location, '/admin/' + newSurveyId);
+        done();
+      });
+    });
+  });
+})
